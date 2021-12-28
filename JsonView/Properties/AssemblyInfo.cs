cb1378528852a1c37e1e9c9845cdb5cdc5cfe1c8
Vincent Tollu <vincent.tollu@orange.com>
2021-12-28 11:07:51 +0100

    zoltu versioning to the rescue

--- JsonView/Properties/AssemblyInfo.cs
+++ JsonView/Properties/AssemblyInfo.cs
@@ -21,13 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bf508807-3b54-45e3-b282-096cc2a0cb45")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
